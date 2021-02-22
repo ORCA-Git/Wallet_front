47f583fd58a9ab0601cec27daf30609c2bef66a3
luzerz <amonchaibeer@gmail.com>
2021-02-23 02:28:56 +0700

    Add Ecosystem Fix port

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 export default {
   //mode: 'universal',
   server :{
-    port: 8080,
+    port: 8000,
   },
   loader: 'less-loader',
   options: { math: { 'parens-division': true, } },
